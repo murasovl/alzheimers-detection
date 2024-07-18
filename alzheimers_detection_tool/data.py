fb57ce5f3f8c21ff092380a5092aa6c20b61ea7a
Moritz Gnann <moritz2012@web.de>
2024-07-18 16:58:42 +0200

    finished api

--- alzheimers_detection_tool/data.py
+++ alzheimers_detection_tool/data.py
@@ -1,5 +1,7 @@
 import numpy as np
 import cv2
+from PIL import Image
+import io
 
 # load_data() preprocessing/resizing/convert if the input test image
 
@@ -9,19 +11,21 @@ import cv2
 def load_data(image_path):
 
     # Intepret as image
-    image = cv2.imread(image_path)
+    image = Image.open(io.BytesIO(image_path))
+    print('image loaded')
     # Convert to RGB
-    image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)  # Convert to RGB format
+    image_rgb = image.convert('RGB')  # Convert to RGB format
     # Resize
-    resized_image = cv2.resize(image_rgb, (224, 224))
+    resized_image = image_rgb.resize((224, 224))
     # Normalizing inside model.
 
     return resized_image
 
 
-def imgage_to_array(resized_image):
+def image_to_array(resized_image):
 
     # Converting the image data to numpy array
     image_array = np.array(resized_image)
+    image_array = image_array.reshape((-1, 224, 224, 3))
 
     return image_array
