1f64e21d0c3ff2f019e676e75f6b629a06c4ee83
Lina Murasov <28linam@gmail.com>
2024-07-19 12:34:28 +0200

    debugged req

--- alzheimers_detection_tool/registry.py
+++ alzheimers_detection_tool/registry.py
@@ -1,11 +1,9 @@
 from tensorflow.keras.models import load_model
 import os
 
-filepath = "/Users/linamurasov/code/murasovl/alzheimers-detection/models/alzheimer_first_draft.keras"
-
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 model_name = "alzheimer_balanced_v4_recall659_prec710.keras"
 
 def load_my_model():
-    model = load_model(os.path.join(root_dir, "model", model_name))
+    model = load_model(os.path.join(root_dir, "models", model_name))
     return model
